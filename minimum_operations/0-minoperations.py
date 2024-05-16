81c428f0ecb961c954aa77f920982fb69950dc6c
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-05-16 13:50:47 +0200

    updated file

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""minimun operations."""
+
+
+def minOperations(n):
+    """Return the fewest number of operations needed."""
+    if n == 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
+    return operations
